e4d41ba7666aa13c67dad76fafb552df39769f00
descalante16 <escalante.desirie1@gmail.com>
2023-02-11 18:07:01 +0800

    close

---
+++ test.js
@@ -0,0 +1,15 @@
+
+// var myArray = ["Washington", "Jessel", "Alicia"];
+
+// myArray.shift();
+// myArray.unshift("Ley");
+
+// console.log(myArray)
+
+function myLocalScope(){
+    var myVar = 5;
+    console.log(myVar);
+}
+
+myLocalScope();
+
